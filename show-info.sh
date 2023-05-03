e17e8e864d91135f9c8b7d9910e8956f35e4fcbe
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-05-03 10:12:28 +0900

    clean: リファクタリング

--- show-info.sh
+++ show-info.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# 表示も見やすくしてないし、結構適当に作ってる
+
 # vim設定行数
 find . -type f | grep -E 'vim$|lua$|toml$|json$' | grep -vE 'colors|not_use' | xargs -I{} cat {} | wc -l
+
 # 使用プラグイン数
 cat toml/dein.toml toml/dein_lazy.toml | grep 'repo = ' | grep -vE '^#' | wc -l
+
 # 起動速度
 # NOTE: [vim-startuptime](https://github.com/rhysd/vim-startuptime)をインストールする必要あり
 vim-startuptime -vimpath nvim
