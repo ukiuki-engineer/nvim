37e0d02cad0d709cef0dffa91d98f0ac8079c0c7
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-05-18 16:58:32 +0900

    clean: リファクタリング

--- show-info.sh
+++ show-info.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # 結構適当
+# 引数に"-startuptime"と入れると、`vim-startuptime -vimpath nvim`を叩く
 
 # vim設定行数
 printf "%20s%s%5s\n" "vim設定行数" " =" $(find . -type f | grep -E 'vim$|lua$|toml$|json$' | grep -vE 'colors|not_use' | xargs -I{} cat {} | wc -l)
@@ -7,11 +8,12 @@ printf "%20s%s%5s\n" "vim設定行数" " =" $(find . -type f | grep -E 'vim$|lua
 # 使用プラグイン数
 printf "%20s%s%5s\n" "使用プラグイン数" " =" $(cat toml/dein.toml toml/dein_lazy.toml | grep 'repo = ' | grep -vE '^#' | wc -l)
 
-echo "\n"
-
 # 起動速度
 # NOTE: [vim-startuptime](https://github.com/rhysd/vim-startuptime)をインストールする必要あり
 if [[ $* == *-startuptime* ]]; then # 引数なしの場合起動速度測定は実行しない
-  vim-startuptime -vimpath nvim
+  command="vim-startuptime -vimpath nvim"
+  echo ""
+  echo $command
+  $command
 fi
 
