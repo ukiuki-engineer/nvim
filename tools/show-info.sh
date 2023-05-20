8e9b6a2f8e9de973f58f1fd9d27dc74b73204b66
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-05-20 16:48:04 +0900

    update: ディレクトリ構成変更

--- tools/show-info.sh
+++ tools/show-info.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # 結構適当
 # 引数に"-startuptime"と入れると、`vim-startuptime -vimpath nvim`を叩く
 
 # vim設定行数
-printf "%20s%s%5s\n" "vim設定行数" " =" $(find . -type f | grep -E 'vim$|lua$|toml$|json$' | grep -vE 'colors|not_use' | xargs -I{} cat {} | wc -l)
+printf "%20s%s%5s\n" "vim設定行数" " =" $(find .. -type f | grep -E 'vim$|lua$|toml$|json$' | grep -vE 'colors|not_use' | xargs -I{} cat {} | wc -l)
 
 # 使用プラグイン数
-printf "%20s%s%5s\n" "使用プラグイン数" " =" $(cat toml/dein.toml toml/dein_lazy.toml | grep 'repo = ' | grep -vE '^#' | wc -l)
+printf "%20s%s%5s\n" "使用プラグイン数" " =" $(cat ../toml/dein.toml ../toml/dein_lazy.toml | grep 'repo = ' | grep -vE '^#' | wc -l)
 
 # 起動速度
 # NOTE: [vim-startuptime](https://github.com/rhysd/vim-startuptime)をインストールする必要あり
