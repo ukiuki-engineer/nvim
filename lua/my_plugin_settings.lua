b3022a0d0d4bdcf463c8449f768f3517aa78b0cd
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-05-22 23:45:52 +0900

    update: lualineのtablineからbufferlineに乗り換え

--- lua/my_plugin_settings.lua
+++ lua/my_plugin_settings.lua
@@ -71,33 +71,6 @@ M.lua_add_lualine = function()
       lualine_y = {},
       lualine_z = {}
     },
-    tabline = {
-      lualine_a = {'tabs'},
-      lualine_b = {
-        {
-          'filename',
-          file_status = true,      -- Displays file status (readonly status, modified status)
-          newfile_status = false,  -- Display new file status (new file means no write after created)
-          path = 1,                -- 0: Just the filename
-                                   -- 1: Relative path
-                                   -- 2: Absolute path
-                                   -- 3: Absolute path, with tilde as the home directory
-                                   -- 4: Filename and parent dir, with tilde as the home directory
-          shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-                                   -- for other components. (terrible name, any suggestions?)
-          symbols = {
-            modified = '[+]',      -- Text to show when the file is modified.
-            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-            unnamed = '[No Name]', -- Text to show for unnamed buffers.
-            newfile = '[New]',     -- Text to show for newly created file before first write
-          }
-        }
-      },
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {'buffers'}
-    }
   })
 end
 
@@ -309,6 +282,24 @@ M.lua_source_nvim_tree = function()
       ignore_list = {},
     },
   }
+end
+
+--
+-- bufferline.nvim
+--
+M.lua_source_bufferline = function()
+  require('bufferline').setup({
+    options = {
+      -- mode = 'tabs',
+      buffer_close_icon = '×',
+      show_tab_indicators = true,
+      diagnostics = "coc",
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end,
+    },
+  })
 end
 
 --
