ae37df4b72a9dda812266fa5b2958d8461a06314
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-07-02 20:58:10 +0900

    update: 色調整

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,25 +13,27 @@ local function custom_color(bg_color, colorscheme)
 
   local function set_customcolor()
     -- diffview
-    vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { -- NOTE: 不明
-      bg = "#FF0000"
-    })
-    vim.api.nvim_set_hl(0, 'DiffDelete', {              -- 削除された行
-      bg = utils.transparent_color(bg_color, "#C70000", 0.90)
-    })
-    vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', {      -- 行が追加された場合の左側
-      bg = utils.transparent_color(bg_color, "#C70000", 0.90),
-      fg = colorscheme == 'gruvbox' and require("gruvbox.palette").colors.dark2 or utils.transparent_color(bg_color, "#2F2F2F", 0.00)
-    })
-    vim.api.nvim_set_hl(0, 'DiffAdd', {                 -- 追加された行
-      bg = utils.transparent_color(bg_color, "#00A100", 0.85)
-    })
-    vim.api.nvim_set_hl(0, 'DiffChange', {              -- 変更行
-      bg = utils.transparent_color(bg_color, "#B9C42F", 0.80)
-    })
-    vim.api.nvim_set_hl(0, 'DiffText', {                -- 変更行の変更箇所
-      bg = utils.transparent_color(bg_color, "#FD7E00", 0.65)
-    })
+    if colorscheme ~= 'nightfox' then
+      vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { -- NOTE: 不明
+        bg = "#FF0000"
+      })
+      vim.api.nvim_set_hl(0, 'DiffDelete', {              -- 削除された行
+        bg = utils.transparent_color(bg_color, "#C70000", 0.90)
+      })
+      vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', {      -- 行が追加された場合の左側
+        bg = utils.transparent_color(bg_color, "#C70000", 0.90),
+        fg = colorscheme == 'gruvbox' and require("gruvbox.palette").colors.dark2 or utils.transparent_color(bg_color, "#2F2F2F", 0.00)
+      })
+      vim.api.nvim_set_hl(0, 'DiffAdd', {                 -- 追加された行
+        bg = utils.transparent_color(bg_color, "#00A100", 0.85)
+      })
+      vim.api.nvim_set_hl(0, 'DiffChange', {              -- 変更行
+        bg = utils.transparent_color(bg_color, "#B9C42F", 0.80)
+      })
+      vim.api.nvim_set_hl(0, 'DiffText', {                -- 変更行の変更箇所
+        bg = utils.transparent_color(bg_color, "#FD7E00", 0.65)
+      })
+    end
     -- coc.nvim
     vim.api.nvim_set_hl(0, 'CocFadeOut', {
       bg = utils.transparent_color(bg_color, '#ADABAC', 0.50),
@@ -311,6 +313,15 @@ M.lua_add_nightfox = function()
   local bg_color = "#192330" -- :hi Normal
   custom_color(bg_color, "nightfox")
   vim.cmd("colorscheme nightfox")
+end
+
+--
+-- catppuccin.nvim
+--
+M.lua_add_catppuccin = function()
+  local bg_color = "1e1e2e" -- :hi Normal
+  custom_color(bg_color, "catppuccin-mocha")
+  vim.cmd("colorscheme catppuccin")
 end
 
 --
