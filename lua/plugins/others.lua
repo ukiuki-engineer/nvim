d832a5acc779239a05c45226372536d8efdc5a79
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-07-28 16:32:27 +0900

    clean

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -169,9 +169,11 @@ M.buffers = function()
 
   require('telescope.builtin').buffers({
     attach_mappings = function(prompt_bufnr, map)
-      map({"i"}, "<C-d>", function()
-        delete_buf(prompt_bufnr)
-      end)
+      map({"i"}, "<C-d>",
+        function()
+          delete_buf(prompt_bufnr)
+        end
+      )
       return true
     end,
   })
