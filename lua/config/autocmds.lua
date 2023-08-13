8ac648f4c882a92eadbf245c65c5490f879ba3c2
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-08-14 03:30:00 +0900

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,12 +1,13 @@
+local fn = vim.fn
 local augroup = vim.api.nvim_create_augroup
 local au = vim.api.nvim_create_autocmd
 local utils = require("utils")
 
-augroup("my_vimrc", {})
+augroup("my_autocmds", {})
 
 -- phpのインデントは4
 au("FileType", {
-  group = "my_vimrc",
+  group = "my_autocmds",
   pattern = {"php"},
   callback = function()
     utils.setlocal_indent(4)
@@ -15,9 +16,17 @@ au("FileType", {
 
 -- FIXME: markdownだけ何故かインデン4になってしまうので一旦強制的に2に。後で原因を調べる。
 au("FileType", {
-  group = "my_vimrc",
+  group = "my_autocmds",
   pattern = {"markdown"},
   callback = function()
     utils.setlocal_indent(2)
   end
+})
+
+-- 保存時にSession.vimを書き込む
+local pwd_in_startup = fn.expand('$PWD')
+local mksession = 'mksession! ' .. pwd_in_startup .. '/Session.vim'
+au("BufWrite", {
+  group = "my_autocmds",
+  command = mksession
 })
