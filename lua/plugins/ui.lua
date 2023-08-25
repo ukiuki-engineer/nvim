1584fe90c50d77e3b8251f64d31292f8ce1b56a6
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-08-26 05:06:10 +0900

    clean

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,6 +33,7 @@ function M.lua_add_lualine()
     end
   end
 
+  -- git情報を表示するか
   local function show_git_info()
     return vim.g['my#git_infomations'] and vim.g['my#git_infomations'] ~= {}
   end
@@ -64,9 +65,9 @@ function M.lua_add_lualine()
   -- animation {{{
   local frames = {
     "(꜆꜄ ˙꒳˙)꜆꜄꜆ｱﾀﾀﾀﾀﾀﾀﾀ!!",
-    "(꜆꜄꜆˙꒳˙)꜄꜆ ｱﾀﾀﾀﾀﾀﾀﾀ!!",
+    "(꜆꜄꜆˙꒳˙)꜆꜄ ｱﾀﾀﾀﾀﾀﾀﾀ!!",
     "(꜄꜆ ˙꒳˙)꜄꜆꜄ｱﾀﾀﾀﾀﾀﾀﾀ!!",
-    "(꜄꜆꜄˙꒳˙)꜆꜄ ｱﾀﾀﾀﾀﾀﾀﾀ!!",
+    "(꜄꜆꜄˙꒳˙)꜄꜆ ｱﾀﾀﾀﾀﾀﾀﾀ!!",
   }
   local frame_number = 1
   local function atatata()
@@ -149,52 +150,6 @@ function M.lua_add_lualine()
       lualine_y = { 'progress' },
       lualine_z = { 'location' }
     },
-    -- inactive_sections = {
-    --   lualine_a = {},
-    --   lualine_b = {},
-    --   lualine_c = {
-    --     {
-    --       'filename',
-    --       file_status = true,     -- Displays file status (readonly status, modified status)
-    --       newfile_status = false, -- Display new file status (new file means no write after created)
-    --       path = 1,               -- 0: Just the filename
-    --       -- 1: Relative path
-    --       -- 2: Absolute path
-    --       -- 3: Absolute path, with tilde as the home directory
-    --       -- 4: Filename and parent dir, with tilde as the home directory
-    --       shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-    --       -- for other components. (terrible name, any suggestions?)
-    --       symbols = {
-    --         modified = '[+]',      -- Text to show when the file is modified.
-    --         readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-    --         unnamed = '[No Name]', -- Text to show for unnamed buffers.
-    --         newfile = '[New]',     -- Text to show for newly created file before first write
-    --       }
-    --     }
-    --   },
-    --   lualine_x = { 'location' },
-    --   lualine_y = {},
-    --   lualine_z = {}
-    -- },
-    -- tabline = {
-    --   lualine_a = {
-    --     'mode', skkeleton_mode
-    --   },
-    --   lualine_b = {
-    --     'branch',
-    --     function()
-    --       return vim.fn['utils#git_commit_status_text']()
-    --     end,
-    --   },
-    --   lualine_c = {
-    --     function()
-    --       return '┐(ﾟдﾟ┐)└(ﾟдﾟ)┐(┌ﾟдﾟ)┌ ┌(ﾟдﾟ)┘┐(ﾟдﾟ┐)└(ﾟдﾟ)┐'
-    --     end
-    --   },
-    --   lualine_x = { 'buffers' },
-    --   lualine_y = {},
-    --   lualine_z = { 'tabs' }
-    -- }
   })
 end
 
