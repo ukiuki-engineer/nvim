6b27281a3894ce9c4c3b4ec5206aefbe89452d0e
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-09-12 12:32:05 +0900

    clean: codingとothersのplugin類別を再整理

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -167,4 +167,150 @@ function M.lua_source_shfmt()
   g.shfmt_fmt_on_save = 1 -- TODO: zshとかでformat on saveが効かないような...？
 end
 
+--
+-- nvim-treesitter
+--
+function M.lua_source_treesitter()
+  -- NOTE: 逆にデフォルトの方が見やすい場合はtreesitterを適宜オフに設定する
+  require('nvim-treesitter.configs').setup {
+    highlight = {
+      enable = true, -- syntax highlightを有効にする
+      disable = {    -- デフォルトの方が見やすい場合は無効に
+      }
+    },
+    indent = {
+      enable = true
+    },
+    matchup = {
+      -- enable = true,              -- mandatory, false will disable the whole extension
+      -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+    },
+    context_commentstring = {
+      enable = true,
+    },
+    -- ensure_installed = 'all', -- :TSInstall allと同じ
+    ensure_installed = {
+      "awk",
+      "bash",
+      "css",
+      "diff",
+      "dockerfile",
+      "html",
+      "ini",
+      "java",
+      "javascript",
+      "jq",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "php",
+      "phpdoc",
+      "ruby",
+      "scss",
+      "sql",
+      "toml",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "vue",
+    },
+  }
+  -- FIXME: 全部書かずに、追加分だけ書く事はできないのか？
+  require("vim.treesitter.query").set(
+    "markdown",
+    "highlights",
+    [[
+      ;From nvim-treesitter/nvim-treesitter
+      (atx_heading (inline) @text.title)
+      (setext_heading (paragraph) @text.title)
+
+      [
+        (atx_h1_marker)
+        (atx_h2_marker)
+        (atx_h3_marker)
+        (atx_h4_marker)
+        (atx_h5_marker)
+        (atx_h6_marker)
+        (setext_h1_underline)
+        (setext_h2_underline)
+      ] @punctuation.special
+
+      [
+        (link_title)
+        (indented_code_block)
+        (fenced_code_block)
+      ] @text.literal
+
+      [
+        (fenced_code_block_delimiter)
+      ] @punctuation.delimiter
+
+      (code_fence_content) @none
+
+      [
+        (link_destination)
+      ] @text.uri
+
+      [
+        (link_label)
+      ] @text.reference
+
+      [
+        (list_marker_plus)
+        (list_marker_minus)
+        (list_marker_star)
+        (list_marker_dot)
+        (list_marker_parenthesis)
+        (thematic_break)
+      ] @punctuation.special
+
+      [
+        (block_continuation)
+        (block_quote_marker)
+      ] @punctuation.special
+
+      [
+        (backslash_escape)
+      ] @string.escape
+      ; 引用はコメントと同じ色に
+      (block_quote) @comment
+    ]]
+  )
+end
+
+--
+-- indent-blankline.nvim
+--
+function M.lua_source_indent_blankline()
+  require("indent_blankline").setup({
+    show_end_of_line     = true,
+    space_char_blankline = " "
+  })
+end
+
+--
+-- nvim-colorizer.lua
+--
+function M.lua_source_colorizer()
+  require("colorizer").setup({
+    "blade",
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "less",
+    "lua",
+    "markdown",
+    "sass",
+    "scss",
+    "stylus",
+    "toml",
+    "txt",
+    "vim",
+    "vue",
+    "xml",
+  })
+end
+
 return M
