3f76f74ecbf08bf43fd934094885e3cef8f41607
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-02-04 02:06:39 +0900

    change: プラグインごとにファイル分け(git系)

---
+++ lua/plugins/git/diffview.lua
@@ -0,0 +1,85 @@
+local command = vim.api.nvim_create_user_command
+
+--
+
+local M = {}
+
+function M.lua_add()
+  vim.keymap.set('n', '<Right>', function()
+    vim.fn["utils#refresh_git_infomations"](true)
+    vim.cmd([[DiffviewOpen]])
+  end, {})
+  vim.keymap.set('n', '<Down>', "<Cmd>DiffviewFileHistory<CR>", {})
+
+  -- NOTE: keymappingが効かない時用。設定し直して開き直す。
+  --       (何故かMacだけkeymappingが効かない時がある)
+  command('ResettingDiffview', function()
+    vim.cmd([[DiffviewClose]])
+    M.lua_source_diffview()
+    vim.cmd([[DiffviewOpen]])
+  end, {})
+end
+
+function M.lua_source()
+  -- NOTE: luaで書くと上手くいかないのでvimscriptで
+  vim.cmd([[
+    " 表示スタイル(tree/list)をtoggle & git情報を更新
+    function! s:aucmds_on_diffviewopen() abort
+      lua require("diffview.config").actions.listing_style()
+      call utils#refresh_git_infomations()
+    endfunction
+
+    augroup MyDiffviewAuCmds
+      au!
+      au User DiffviewViewOpened call s:aucmds_on_diffviewopen()
+    augroup END
+  ]])
+
+  require('diffview').setup({
+    enhanced_diff_hl = true,
+    file_panel = {
+      win_config = {
+        -- diffviewのwindowの設定
+        type = "split",
+        position = "right",
+        width = 40,
+      },
+    },
+    keymaps = {
+      file_panel = {
+        -- 効いたり効かなかったりする {{{
+        -- commit
+        { "n", "<Down>",
+          function()
+            vim.cmd([[Gin commit]])
+          end,
+          { desc = "execute :Gin commit" }
+        },
+        -- confirmしてpushする
+        { "n", "<Up>",
+          function()
+            require("plugins.git.gin").pcall_git_push_confirm()
+          end,
+          { desc = "confirm -> :Gin push" }
+        },
+        -- }}}
+        -- confirmして変更を削除する
+        { "n", "X",
+          function()
+            local message = "Delete this changes?"
+            if not vim.fn["utils#confirm"](message) then
+              return
+            end
+            -- restore
+            require("diffview.config").actions.restore_entry()
+            -- git情報を更新
+            vim.fn["utils#refresh_git_infomations"]()
+          end,
+          { desc = "confirm -> restore" }
+        },
+      }
+    }
+  })
+end
+
+return M
---
+++ lua/plugins/git/gin.lua
@@ -0,0 +1,100 @@
+--
+-- gin.vim
+--
+local g       = vim.g
+local fn      = vim.fn
+local command = vim.api.nvim_create_user_command
+local utils   = require("utils")
+
+-- confirmしてpushする
+local function git_push_confirm()
+  vim.fn['utils#refresh_git_infomations']()
+
+  local message = ""
+
+  -- remote branchが無い場合の処理
+  if not g['my#git_infomations']['exists_remote_branch'] then
+    message = 'There is no remote branch for the \"' ..
+        g['my#git_infomations']['branch_name'] .. '\". Would you like to publish this branch?'
+
+    if vim.fn["utils#confirm"](message) then
+      vim.cmd("Gin push --set-upstream origin HEAD")
+    end
+    return
+  end
+
+  local commit_count = g['my#git_infomations']['commit_count']['local']
+  commit_count = tonumber(commit_count)
+
+  if commit_count == "" or commit_count == 0 then
+    print("no commits")
+    return
+  end
+
+  message = commit_count == 1
+      and "push " .. commit_count .. " commit?"
+      or "push " .. commit_count .. "commits?"
+
+  if vim.fn["utils#confirm"](message) then
+    vim.cmd([[Gin push]])
+  end
+end
+
+-- confirmしてgit resetする
+local function delete_latest_commit(soft_or_hard)
+  if not vim.fn["utils#confirm"]("Delete latest commit?") then
+    return
+  end
+  vim.cmd("Gin reset --" .. soft_or_hard .. " HEAD^")
+  -- NOTE: 普通にcommand実行するだけだとなんか時々上手くいかないのでtimer遅延をかける
+  fn.timer_start(1000,
+    function()
+      -- diffviewをrefresh
+      vim.cmd([[DiffviewRefresh]])
+    end
+  )
+end
+
+--
+
+local M = {}
+
+function M.lua_add()
+  -- 画面分割して開く
+  g.gin_proxy_editor_opener = 'split'
+
+  vim.cmd([[
+    augroup MyGinAuCmds
+      au!
+      au User GinCommandPost call utils#refresh_git_infomations()
+      au User GinComponentPost call utils#refresh_git_infomations()
+    augroup END
+  ]])
+
+  -- commands
+  command('DeleteLatestCommit', function() M.pcall_delete_latest_commit('soft') end, {})
+  command('GinPush', M.pcall_git_push_confirm, {})
+  -- TODO: 引数を渡せるようにする。↓みたいな感じでいけるらしい。
+  -- vim.cmd("command! -nargs=? GinPush call luaeval('M.pcall_git_push_confirm(_A)', <q-args>)")
+end
+
+--------------------------------------------------------------------------------
+-- プラグイン設定以外で外部に公開するfunctions
+--------------------------------------------------------------------------------
+-- git_push_confirm()をpcallでラップして実行
+function M.pcall_git_push_confirm()
+  local success, exception = pcall(git_push_confirm)
+  if not success then
+    utils.echo_error_message(exception, "E006")
+  end
+end
+
+-- delete_latest_commit()をpcallでラップして実行
+function M.pcall_delete_latest_commit(soft_or_hard)
+  local success, exception = pcall(delete_latest_commit, soft_or_hard)
+  if not success then
+    utils.echo_error_message(exception, "E007")
+  end
+end
+
+return M
