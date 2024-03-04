39697d05866c0bbe2ca14e087f852299c8bc9d4c
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-03-04 20:29:31 +0900

    update

--- lua/plugins/coding/edgemotion.lua
+++ lua/plugins/coding/edgemotion.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.lua_source()
-  vim.keymap.set('n', '<C-j>', '<Plug>(edgemotion-j)', { silent = true })
-  vim.keymap.set('n', '<C-k>', '<Plug>(edgemotion-k)', { silent = true })
+  vim.keymap.set({ 'n', 'x' }, '<C-j>', '<Plug>(edgemotion-j)', { silent = true })
+  vim.keymap.set({ 'n', 'x' }, '<C-k>', '<Plug>(edgemotion-k)', { silent = true })
 end
 
 return M
