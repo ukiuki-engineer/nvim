3ce1b5ae40aa7c2d4c056dc844e8e07733cddc09
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-04-03 10:57:10 +0900

    add: nvim-autopairs

---
+++ lua/plugins/coding/nvim-autopairs.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.lua_source()
+  require("nvim-autopairs").setup({
+    -- NOTE: 一旦デフォルト値をそのまま入れる
+    disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    disable_in_macro = true,
+    disable_in_visualblock = false,
+    disable_in_replace_mode = true,
+    ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+    enable_moveright = true,
+    enable_afterquote = true,
+    enable_check_bracket_line = true,
+    enable_bracket_in_quote = true,
+    enable_abbr = false,
+    break_undo = true,
+    check_ts = false,
+    map_cr = true,
+    map_bs = true,
+    map_c_h = false,
+    map_c_w = false,
+  })
+end
+
+return M
