6c1200a176f8bf7b18ab0f8faa5bc33558fba044
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-04-03 10:56:23 +0900

    clean: 整理

--- lua/plugins/coding/shfmt.lua
+++ lua/plugins/coding/shfmt.lua
@@ -6,7 +6,9 @@ function M.lua_source()
     vim.fn.system("go install mvdan.cc/sh/v3/cmd/shfmt@latest")
   end
   vim.g.shfmt_extra_args  = '-i 2 -ci -bn -s'
-  vim.g.shfmt_fmt_on_save = 1 -- TODO: zshとかでformat on saveが効くようにできないだろうか？
+  -- NOTE: *.zshでも、:Shfmtで実行はできる
+  --       が、zsh固有の構文に対応してないからエラーになったりする
+  vim.g.shfmt_fmt_on_save = 1
 end
 
 return M
