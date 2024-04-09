87fb1cf696c0bd30c375c1ff238fa9f6a2cc27be
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-04-09 14:38:45 +0900

    update: autopairsの設定

--- lua/plugins/coding/nvim-autopairs.lua
+++ lua/plugins/coding/nvim-autopairs.lua
@@ -6,7 +6,7 @@ function M.lua_source()
     disable_in_macro = true,
     disable_in_visualblock = false,
     disable_in_replace_mode = true,
-    ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+    ignored_next_char = "",
     enable_moveright = true,
     enable_afterquote = true,
     enable_check_bracket_line = true,
