674c9c7bf8b94f83a9182af4369a56b82a3eb670
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-06-10 17:54:41 +0900

    update: WSLでクリップボード連携が上手くいかなかったので修正

--- lua/config/lazy/clipboard.lua
+++ lua/config/lazy/clipboard.lua
@@ -8,18 +8,7 @@ if require("utils").is_wsl() then
   vim.fn.timer_start(
     vim.g["my#const"].timer_start_clipboard,
     function()
-      vim.g.clipboard = {
-        name = 'WslClipboard',
-        copy = {
-          ['+'] = 'clip.exe',
-          ['*'] = 'clip.exe',
-        },
-        paste = {
-          ['+'] = 'powershell.exe -c "[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace(\"`r\", \"\"))"',
-          ['*'] = 'powershell.exe -c "[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace(\"`r\", \"\"))"',
-        },
-        cache_enabled = 0,
-      }
+      vim.cmd([[set clipboard=unnamedplus]])
     end
   )
 else
