cccb43514f851c3142367c6ae188f595d55026cd
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-07-19 13:37:14 +0900

    change: tokyonight系の色を微調整

--- lua/plugins/colorscheme/tokyonight.lua
+++ lua/plugins/colorscheme/tokyonight.lua
@@ -18,28 +18,22 @@ function M.set_customcolor()
   -- background colorを取得
   local bg_color          = colorscheme_utils.get_background()
 
-  -- hi(0, 'Comment', { fg = "#565f89" })
+  -- コメント
   hi(0, 'Comment', { fg = "#8a92b6" })
 
-  -- hi(0, 'Visual', { bg = "#283457" })
+  -- ビジュアル行
   hi(0, 'Visual', { bg = "#394b7d" })
-  hi(0, 'CursorLine', {
-    bg = colorscheme_utils.transparent_color(bg_color, "Magenta", 0.60),
-  })
-  hi(0, 'CursorColumn', { link = "CursorLine" })
 
   -- 行番号
   hi(0, 'LineNr', { fg = "#515a84" })
-  -- 行番号(カーソル行)
-  hi(0, 'CursorLineNr', { fg = "#959ab8" })
+
   -- 差分
   colorscheme_utils.hi_diff(bg_color)
 
   -- ヤンク範囲
   hi(0, 'HighlightedyankRegion', {
-    bg = colorscheme_utils.transparent_color(bg_color, "Yellow", 0.60),
+    bg = colorscheme_utils.transparent_color(bg_color, "#ff9e64", 0.60),
   })
-  hi(0, 'CocMenuSel', { link = "Visual" })
 
   -- vim-matchup
   hi(0, 'MatchParen', {
