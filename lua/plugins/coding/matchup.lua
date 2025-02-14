7a242269effb718e23018bf57fe023198018244d
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2025-02-14 13:25:52 +0900

    update: matchupの設定

--- lua/plugins/coding/matchup.lua
+++ lua/plugins/coding/matchup.lua
@@ -2,10 +2,13 @@ local M = {}
 
 function M.lua_source()
   vim.g.matchup_matchpref = {
-    html  = { tagnameonly = 1 },
-    xml   = { tagnameonly = 1 },
-    blade = { tagnameonly = 1 },
-    vue   = { tagnameonly = 1 }
+    html            = { tagnameonly = 1 },
+    xml             = { tagnameonly = 1 },
+    blade           = { tagnameonly = 1 },
+    vue             = { tagnameonly = 1 },
+    typescriptreact = { tagnameonly = 1 },
+    javascript      = { tagnameonly = 1 },
+    javascriptreact = { tagnameonly = 1 },
   }
 end
 
