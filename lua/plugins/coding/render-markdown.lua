309eea5faa26bc349b6fc188b126193342b1feb7
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2026-05-13 13:07:59 +0900

    update

--- lua/plugins/coding/render-markdown.lua
+++ lua/plugins/coding/render-markdown.lua
@@ -2,6 +2,20 @@ local M = {}
 
 function M.lua_source()
   require('render-markdown').setup({
+    anti_conceal = {
+      -- カーソル行の描画キャンセル
+      enabled = false,
+    },
+    heading = {
+      position = 'inline',
+      left_margin = 0,
+      left_pad = 0,
+      right_pad = 0,
+      width = 'block',
+    },
+    indent = {
+      enabled = false,
+    },
   })
 end
 
